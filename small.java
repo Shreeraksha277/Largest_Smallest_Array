d28beb63709b669f8a78b4138cb8e2eff29c77f5
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2025-12-18 17:53:58 +0530

    Implement function to find the smallest element in array

---
+++ small.java
@@ -0,0 +1,10 @@
+public static int findSmallest(int[] arr) {
+    int min = arr[0];   // assume first element is smallest
+
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
