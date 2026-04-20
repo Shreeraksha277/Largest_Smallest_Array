c99b178409a4e377db87ab0c669d0b3b32e082b9
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-04-20 12:26:06 +0530

    Update small.java

--- small.java
+++ small.java
@@ -1,5 +1,5 @@
 public static int findSmallest(int[] arr) {
-      int min = arr[0] ;   // assume first element is smallest
+       int min = arr[0] ;   // assume first element is smallest
  
           for (int i = 1; i < arr.length; i++) {
           if (arr[i] < min) {
